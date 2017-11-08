70df3ed5b640828529f70853fa2ad14f5be1dd82
minghorn <minghaihorn@gmail.com>
2017-11-08 13:55:55 -0800

    Grade logic

--- calcgrade.asm
+++ calcgrade.asm
@@ -12,18 +12,36 @@ again:
  ; your codes go here.
  exit
 main ENDP
+
 CalcGrade proc
 ; Calculates a letter grade
 ; Receives: EAX = numeric grade
 ; Returns: AL = letter grade
+
+JMP Grade_A
 Grade_A:
-; your codes go here.
+	CMP EAX, 90
+	mov AL, 'A'
+	JGE finished
+	JMP Grade_B
 Grade_B:
-; your codes go here.
-;Repeat for Grade_C, Grade_D, and Grade_F
+	CMP EAX, 80
+	mov AL, 'A'
+	JGE finished
+	JMP Grade_C
 Grade_C:
+	CMP EAX, 70
+	mov AL, 'A'
+	JGE finished
+	JMP Grade_D
 Grade_D:
+	CMP EAX, 60
+	mov AL, 'D'
+	JGE finished
+	JMP Grade_F
 Grade_F:
+	mov AL, 'F'
+	JGE finished
 finished:
  ret
 CalcGrade endp
